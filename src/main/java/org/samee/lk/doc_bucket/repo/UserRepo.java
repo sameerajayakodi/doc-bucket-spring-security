c630a723f0bc356dde830a286bada32e6af92bc9
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2025-01-27 16:40:31 +0530

    create UserRepo.java

--- src/main/java/org/samee/lk/doc_bucket/repo/UserRepo.java
+++ src/main/java/org/samee/lk/doc_bucket/repo/UserRepo.java
@@ -3,6 +3,9 @@ package org.samee.lk.doc_bucket.repo;
 import org.samee.lk.doc_bucket.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User,String> {
     boolean existsByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
